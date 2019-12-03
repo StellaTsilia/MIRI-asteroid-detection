bb5854a7f41ce4971084f6999c0e49416f89d728
StellaTsilia <stella.tsilia@gmail.com>
2019-12-03 15:48:08 +0200

    updated code to save plts

--- Code to generate plots 1.py
+++ Code to generate plots 1.py
@@ -11,7 +11,7 @@ import numpy as np
 plot2d=np.array(data[0,14,535:560,695:720])        #source located in ds9
 hdu=fits.PrimaryHDU(plot2d)                        #saved to a new file
 hdul=fits.HDUList([hdu])
-hdul.writeto("plot2d")
+hdul.writeto("plot2d.fits")
 
 plot1d=np.array(data[0,:,547,707])                 #one of the four brightest pixels
 x_axis=np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
@@ -23,9 +23,12 @@ imgplot=plt.imshow(plot2d)
 plt.imshow(plot2d,cmap="hot")
 plt.colorbar()
 plt.title("2D Plot of source's brightness")        #export plot
+plt.savefig("plot2d.png")
+plt.close()
 
 plt.plot(x_axis,plot1d,'r')
 plt.xlabel("frame number")
 plt.ylabel("pixel value")
 plt.title("Flux from 1 pixel of the source")       #export plot
+plt.savefig("plot1d.png")
 
