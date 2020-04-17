a16754506fa391575002496f263d9968c0bb90e1
StellaTsilia <stella.tsilia@gmail.com>
2020-04-17 12:59:51 +0200

    updated script to include header

--- move_source_code.py
+++ move_source_code.py
@@ -6,24 +6,25 @@ import math
 
 import function_move_source as fl
 
-flag_collapse_source=True # True if script is used to collapse a source
-input1="/home/stella/point_sources/sim1/det_images/mv_source_final.fits" # path of the input file
-output1="/home/stella/point_sources/sim1/det_images/col_source_final.fits" # path of the output file
+flag_collapse_source=False # True if script is used to collapse a source
+input1="/home/stella/point_sources/sim1/det_images/det_image_seq1_MIRIMAGE_F1280Wexp1.fits" # path of the input file
+output1="/home/stella/point_sources/sim1/det_images/mv_source_final.fits" # path of the output file
 vel=3 # velocity in pixels/frame 
 theta=math.pi/6 # angle (rads) between source's direction and positive x-axis  
 
 
 hdul=fits.open(input1)
-if flag_collapse_source:
-	data=hdul[0].data  
+hdr=hdul[0].header
+data=hdul[1].data
+
+if flag_collapse_source: 
 	theta=theta-math.pi
-else:
-	data=hdul[1].data
 
 mv_source_final=fl.move_source(data,vel,theta)
 
-hdu=fits.PrimaryHDU(mv_source_final)
-hdul=fits.HDUList([hdu])
+primary_hdu=fits.PrimaryHDU(header=hdr)
+image_hdu=fits.ImageHDU(mv_source_final)
+hdul=fits.HDUList([primary_hdu,image_hdu])
 hdul.writeto(output1)
 	
 
