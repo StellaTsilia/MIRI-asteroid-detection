8ae3251a1e84ee9991996999003db9a0871037eb
StellaTsilia <stella.tsilia@gmail.com>
2020-04-22 19:58:58 +0200

    latest version, trying to work with the pipeline

--- move_source_code.py
+++ move_source_code.py
@@ -6,25 +6,27 @@ import math
 
 import function_move_source as fl
 
-flag_collapse_source=False # True if script is used to collapse a source
+flag_move_source=True # True if script is used to move a source
 input1="/home/stella/point_sources/sim1/det_images/det_image_seq1_MIRIMAGE_F1280Wexp1.fits" # path of the input file
 output1="/home/stella/point_sources/sim1/det_images/mv_source_final.fits" # path of the output file
 vel=3 # velocity in pixels/frame 
-theta=math.pi/6 # angle (rads) between source's direction and positive x-axis  
+theta=math.pi/6-math.pi # angle (rads) between source's direction and positive x-axis  
 
 
 hdul=fits.open(input1)
-hdr=hdul[0].header
 data=hdul[1].data
+pixeldq=hdul[2].data
 
-if flag_collapse_source: 
-	theta=theta-math.pi
+if flag_move_source: 
+	theta=theta+math.pi
 
 mv_source_final=fl.move_source(data,vel,theta)
 
-primary_hdu=fits.PrimaryHDU(header=hdr)
-image_hdu=fits.ImageHDU(mv_source_final)
-hdul=fits.HDUList([primary_hdu,image_hdu])
+hdul[1].data=mv_source_final
+
+nz=mv_source_final.shape[1]-1	
+mask=np.where(np.isnan(mv_source_final[0,nz,:,:]),1,0)
+hdul[2].data=np.where(pixeldq==0,mask,pixeldq)
+
 hdul.writeto(output1)
-	
 
