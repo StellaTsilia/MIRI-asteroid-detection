b4965494697374511f72cea441630e8bb0c40e3e
StellaTsilia <stella.tsilia@gmail.com>
2020-07-27 23:46:23 +0300

    update
    changed velocity's input units from pixels/frame to arcsec/sec and commented the code

--- change_pixeldq.py
+++ change_pixeldq.py
@@ -1,21 +1,25 @@
+# tsilia@mail.strw.leidenuniv.nl  
+# 2020/07/27
+
 import astropy
 from astropy.io import fits
 
 import numpy as np
 import math
 
+input2="/home/stella/point_sources/2006_QG93_40f/det_images/col_source_final_dqinitstep.fits" # path of the input file
+output2="/home/stella/point_sources/2006_QG93_40f/det_images/col_source_pipeline.fits" # path of the output file
 
-input2="/home/stella/point_sources/sim1/det_images/mv_source_final_dqinitstep.fits" # path of the input file
-output2="/home/stella/point_sources/sim1/det_images/mv_source_pipeline.fits" # path of the output file
-
-
+# open FITS file
 hdul=fits.open(input2)
 data=hdul[1].data
 pixeldq=hdul[2].data
 
+# apply mask 
 nz=data.shape[1]-1	
 mask=np.where(np.isnan(data[0,nz,:,:]),1,0)
 hdul[2].data=np.where(pixeldq==0,mask,pixeldq)
 
+# save data
 hdul.writeto(output2)
 
--- move_source_code.py
+++ move_source_code.py
@@ -1,3 +1,6 @@
+# tsilia@mail.strw.leidenuniv.nl  
+# 2020/07/27
+
 import astropy
 from astropy.io import fits
 
@@ -6,22 +9,28 @@ import math
 
 import function_move_source as fl
 
+pixel_scale=0.11 # pixel scale of the instrument in arcsec/pixel
+t_1=2.775 # time between individual frames in sec
 
-flag_move_source=True # True if script is used to move a source
-input1="/home/stella/point_sources/sim1/det_images/det_image_seq1_MIRIMAGE_F1280Wexp1.fits" # path of the input file
-output1="/home/stella/point_sources/sim1/det_images/mv_source_final.fits" # path of the output file
-vel=3 # velocity in pixels/frame 
-theta=math.pi/6-math.pi # angle (rads) between source's direction and positive x-axis  
+flag_move_source=False # True if script is used to generate a moving source out of a stationary one
+input1="/home/stella/point_sources/2006_QG93_40f/det_images/mv_source_final.fits" # path of the input file
+output1="/home/stella/point_sources/2006_QG93_40f/det_images/col_source_final.fits" # path of the output file
+vel_arc=0.010996 # velocity in arcsec/sec 
+theta=math.pi # angle (rads) between source's direction and positive x-axis  
 
+vel=vel_arc*(t_1/pixel_scale) # convert velocity from arcsec/sec to pixels/frame
 
+# open FITS file 
 hdul=fits.open(input1)
 data=hdul[1].data
 
 if flag_move_source: 
 	theta=theta+math.pi
 
+# run function
 mv_source_final=fl.move_source(data,vel,theta)
 hdul[1].data=mv_source_final
 
+# save results
 hdul.writeto(output1)
 
